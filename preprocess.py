6a76e692d440ad1e1fa374f309e5f9cc135df4fa
Diogo Pereira <diogo99p@gmail.com>
2020-02-11 13:36:09 +0000

    tokenizer done

--- preprocess.py
+++ preprocess.py
@@ -1,6 +1,28 @@
 import pandas as pd
+from spacy.tokenizer import Tokenizer
+from spacy.lang.en import English
+import string
 
-data = pd.read_csv('TA_restaurants_curated.csv')
+df = pd.read_table('TripAdvisorUKRestaurant-max_MF.txt')
 
-global_number_rest = data['City'].value_counts(dropna=False)
-print(global_number_rest)
+ratings = df.iloc[:,0]
+reviews = df.iloc[:,1]
+
+ratings_distribution = ratings.value_counts()
+
+print(ratings_distribution)
+print(len(reviews))
+
+nlp = English()
+tokenizer = Tokenizer(nlp.vocab)
+	
+table = str.maketrans('', '', string.punctuation)
+
+for review in reviews:
+	tokens = tokenizer(review)
+	for token in tokens:
+		print(token.orth_)
+	stripped = [token.orth_ for token in tokens if not token.is_punct | token.is_space]
+	for token in stripped:
+		print(token)
+	break
