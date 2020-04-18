e4a16f417d04dd45caec7de25d5fdfe8195e1da3
Diogo Pereira <diogo99p@gmail.com>
2020-04-18 22:23:09 +0100

    Added gpu support for BERT and added new metric

--- Deep/BERT.py
+++ Deep/BERT.py
@@ -3,38 +3,31 @@ import torch
 import pandas as pd
 from torch.utils.data import TensorDataset, random_split, DataLoader
 import numpy as np 
-import progressbar
+from progress.bar import IncrementalBar
 import time
 import datetime
+from sklearn.metrics import matthews_corrcoef
+
+device = "cuda:0" if torch.cuda.is_available() else "cpu"
 
 def getData():
     df = pd.read_csv('../kindle_reviews.csv', keep_default_na=False)
     df = df.rename(columns={'Unnamed: 0': 'Id'})
-    
+
     return df
 
 def tokenize(df):
-    print("Started Tokenizer")
-    print("-----------------")
+    print("**Started Tokenizer**")
     tokenizer = transformers.BertTokenizer.from_pretrained('bert-base-cased', do_lower_case=True)
 
     ids = []
     masks = []
     labels = []
 
-    dfsize = len(df)
-    barsize = int(dfsize/100000)
-
-    bar = progressbar.ProgressBar(maxval=barsize, \
-    widgets=[progressbar.Bar('=', '[', ']'), ' ', progressbar.Percentage()])
-    bar.start()
-
-    curr_sample = 0
+    bar = IncrementalBar('Review', max = len(df))
 
     for review, label in zip(df['reviewText'], df['overall']):
-        if dfsize%100000 == 0:
-            bar.update(curr_sample+1)
-            curr_sample += 1
+        bar.next()
         encoded = tokenizer.encode_plus(review, add_special_tokens= True,
                             max_length = 512, pad_to_max_length = True,
                             return_attention_mask= True, return_tensors='pt')
@@ -42,16 +35,14 @@ def tokenize(df):
         ids.append(encoded['input_ids'])
         masks.append(encoded['attention_mask'])
         labels.append(label - 1)
-    
-    bar.update(barsize)
+
     bar.finish()
 
     ids = torch.cat(ids)
     masks = torch.cat(masks)
     labels = torch.tensor(labels)
 
-    print("-----------------")
-    print("Finished Tokenizer\n\n")
+    print("**Finished Tokenizer**\n\n")
     return ids, masks, labels
 
 def split_data(ids, masks, labels):
@@ -59,8 +50,9 @@ def split_data(ids, masks, labels):
     size = len(dataset)
 
     train_size = int(0.8*size)
-    val_test_size = int(0.1*size)
-    train, val, test = random_split(dataset, [train_size, val_test_size, val_test_size])
+    val_size = int(0.1*size)
+    test_size = size - train_size - val_size
+    train, val, test = random_split(dataset, [train_size, val_size, test_size])
 
     train_data = DataLoader(train, batch_size=128, shuffle=True)
     val_data = DataLoader(val, batch_size=128, shuffle=True)
@@ -75,9 +67,18 @@ def accuracy(labels, predictions):
 
     return np.sum(predictions == labels) / size
 
+def mcc(labels, predictions):
+    labels = np.concatenate(labels, axis=0)
+
+    predictions = np.concatenate(predictions, axis=0)
+    predictions = np.argmax(predictions, axis=1).flatten()    
+
+    return matthews_corrcoef(labels, predictions)
+
+
 def training(train_data, val_data, test_data):
-    print("Started Training")
-    print("-----------------")
+    print("**Started Training**")
+    print("Training using " + device)
     model = transformers.BertForSequenceClassification.from_pretrained(
         "bert-base-cased", num_labels = 5,output_attentions = False, output_hidden_states = False)
     
@@ -87,25 +88,23 @@ def training(train_data, val_data, test_data):
     scheduler = transformers.get_linear_schedule_with_warmup(optimizer, 
                                             num_warmup_steps = 0,
                                             num_training_steps = len(train_data) * epochs)
-    
-    bar = progressbar.ProgressBar(maxval=len(train_data), \
-    widgets=[progressbar.Bar('=', '[', ']'), ' ', progressbar.Percentage()])
 
     for epoch in range(epochs):
         total = time.time()
-        print("Epoch " + str(epoch + 1) + " progress:")
-        bar.start()
+        print("\nEpoch " + str(epoch + 1) + "/" + str(epochs))
         train_loss = 0
         curr_batch = 0
         model.train()
 
+        bar = IncrementalBar('Batch', max = len(train_data))
+
         for step, batch in enumerate(train_data):
-            bar.update(curr_batch+1)
-            curr_batch += 1
+            bar.next()
             model.zero_grad()
-            loss, logits = model(batch[0], token_type_ids=None,
-                            attention_mask=batch[1], 
-                            labels=batch[2])
+
+            loss, logits = model(batch[0].to(device), token_type_ids=None,
+                            attention_mask=batch[1].to(device), 
+                            labels=batch[2].to(device))
             train_loss += loss.item()
             loss.backward()
 
@@ -114,9 +113,9 @@ def training(train_data, val_data, test_data):
 
             optimizer.step()
             scheduler.step()
-    
+        
         total = time.time() - total
-        print("Elapsed Time: " + str(datetime.timedelta(seconds=int(round((total))))))
+        print("\nElapsed Time: " + str(datetime.timedelta(seconds=int(round((total))))))
         print("Average training loss: " + str(train_loss/len(train_data)))
         bar.finish()
 
@@ -125,39 +124,53 @@ def training(train_data, val_data, test_data):
         val_acc = 0
         val_loss = 0
 
+        print("\n**Running Validation batches**")
+        bar = IncrementalBar('Batch', max = len(val_data))
         for batch in val_data:
-            while(torch.no_grad()):
-                loss, logits = model(batch[0], token_type_ids=None,
-                                attention_mask=batch[1], 
-                                labels=batch[2])
+            bar.next()
+
+            with torch.no_grad():
+                loss, logits = model(batch[0].to(device), token_type_ids=None,
+                                attention_mask=batch[1].to(device), 
+                                labels=batch[2].to(device))
             
+            logits = logits.detach().cpu().numpy()
             val_loss += loss.item()
-            val_acc += accuracy(batch[2], logits)
+            val_acc += accuracy(batch[2].numpy(), logits)
         
+        bar.finish()
         print("Average validation loss: " + str(val_loss/len(val_data)))
         print("Average validation accuracy: " + str(val_acc/len(val_data)))
 
-    print("-----------------")
-    print("Ended Training")
+    print("**Ended Training**\n")
+    torch.save(model.state_dict(), 'Bert_trained.pth')
 
-    print("\nStarted Testing")
+    print("\n**Started Testing**")
     print("-----------------")
 
     model.eval()
 
     test_acc = 0
 
-    for batch in test_data:
-        while(torch.no_grad()):
-            _, logits = model(batch[0], token_type_ids=None,
-                            attention_mask=batch[1], 
-                            labels=batch[2])
-        
-        test_acc += accuracy(batch[2], logits)
+    true_labels = []
+    predicted_labels = []
 
-    print("Average test accuracy: " + str(val_acc/len(test_data)))
-    print("-----------------")
-    print("Ended Testing")
+    for batch in test_data:
+        with torch.no_grad():
+            output = model(batch[0].to(device), token_type_ids=None,
+                            attention_mask=batch[1].to(device))
+        
+        logits = output[0]
+        logits = logits.detach().cpu().numpy()
+
+        true_labels.append(batch[2].numpy())
+        predicted_labels.append(logits)
+
+        test_acc += accuracy(batch[2].numpy(), logits)
+
+    print("Test Matthews correlation coefficient: " + str(mcc(true_labels, predicted_labels)))
+    print("Average test accuracy: " + str(test_acc/len(test_data)))
+    print("**Ended Testing**")
 
 
 if __name__ == "__main__":
