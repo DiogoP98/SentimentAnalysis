b46995ae635aec404018f78b99c754767d14ca32
Diogo Pereira <diogo99p@gmail.com>
2020-05-11 15:23:39 +0100

    checkpoints in only one file

--- Deep/FineTuning.py
+++ Deep/FineTuning.py
@@ -2,44 +2,18 @@ import transformers
 import torch
 import utils
 from torch.utils.data import TensorDataset, random_split, DataLoader
-import numpy as np 
-from progress.bar import IncrementalBar
+import numpy as np
 import time
 import datetime
 from transformers import XLNetModel, XLNetTokenizer, XLNetForSequenceClassification
 from transformers import BertModel, BertTokenizer, BertForSequenceClassification
 from torch import nn
 import sys
+import os
+from tqdm import tqdm
 
 device = "cuda:0" if torch.cuda.is_available() else "cpu"
 num_classes = 5
-
-def setup_BERT():
-    tokenizer = BertTokenizer.from_pretrained('bert-base-cased', do_lower_case=True)
-    model = BertForSequenceClassification.from_pretrained("bert-base-cased", num_labels=num_classes, output_attentions = False, output_hidden_states = False)
-    #model.classifier = nn.Linear(768, num_classes)
-
-    #for param in model.parameters():
-    #    param.requires_grad = False
-    
-    #model.classifier.weight.requires_grad = True #unfreeze last layer weights
-    #model.classifier.bias.requires_grad = True #unfreeze last layer biases
-    model = model.to(device)
-
-    return model, tokenizer
-
-def setup_XLNet():
-    tokenizer = XLNetTokenizer.from_pretrained('xlnet-base-cased', do_lower_case=True)
-    model = XLNetForSequenceClassification.from_pretrained("xlnet-base-cased", num_labels=num_classes, output_attentions = False, output_hidden_states = False)
-    #model.logits_proj = nn.Linear(768, num_classes)
-
-    #for param in model.parameters():
-    #    param.requires_grad = False
-    #model.logits_proj.weight.requires_grad = True #unfreeze last layer weights
-    #model.logits_proj.bias.requires_grad = True #unfreeze last layer biases
-    model = model.to(device)
-
-    return model, tokenizer
 
 def tokenize(df, tokenizer):
     ids = []
@@ -93,10 +67,13 @@ def split_data(ids, masks, labels):
 
     return train_data, val_data, test_data
 
-def fine_tune(model, train_data, val_data, selected_model):
+def fine_tune(model, train_data, val_data, selected_model, use_checkpoint):
     print("**Started Fine Tune**")
     print("Using " + device)
-    
+    epoch = 0
+
+
+
     #Adam optimizer with weight decay fix
     optimizer = transformers.AdamW(filter(lambda p: p.requires_grad, model.parameters()), lr = 5e-5, eps = 1e-8)
     epochs = 2
@@ -104,22 +81,41 @@ def fine_tune(model, train_data, val_data, selected_model):
                                             num_warmup_steps = 0,
                                             num_training_steps = len(train_data) * epochs)
 
-    for epoch in range(epochs):
+    start_epoch = 0
+    if use_checkpoint:
+        model, optimizer, scheduler, start_epoch, batch_num = load_checkpoint(model, optimizer, scheduler, selected_model)
+        model = model.to(device)
+        # now individually transfer the optimizer parts...
+        for state in optimizer.state.values():
+            for k, v in state.items():
+                if isinstance(v, torch.Tensor):
+                    state[k] = v.to(device)
+
+        # reload the dataloader
+        train_data = torch.load(dir_path + "train_dataloader.pth")
+        val_data = torch.load(dir_path + "val_dataloader.pth")
+       
+
+    for epoch in range(start_epoch, epochs):
+        if epoch > start_epoch and use_checkpoint == True:
+            use_checkpoint = False
         total = time.time()
         print("\nEpoch " + str(epoch + 1) + "/" + str(epochs))
-        train_loss = 0
-        curr_batch = 0
+        train_loss = batch_n = 0
         model.train()
+        
 
-        bar = IncrementalBar('Batch', max = len(train_data))
-        batch_n = 0
-
-        for step, batch in enumerate(train_data):
+        for step, batch in enumerate(tqdm(train_data)):
+            if use_checkpoint:
+                if step < batch_num:
+                    continue
             batch_n += 1
-            if batch_n % 10 == 0:
-                utils.checkpoint(model, optimizer, scheduler, selected_model)
+            if batch_n % 100 == 0:
+                utils.checkpoint(model, optimizer, scheduler, epoch, batch_n, selected_model, model_path)
+                # save the dataloader
+                torch.save(train_data, dir_path + 'train_dataloader.pth')
+                torch.save(val_data, dir_path + 'val_dataloader.pth')
             
-            bar.next()
             optimizer.zero_grad()
 
             loss, logits = model(batch[0].to(device), token_type_ids=None,
@@ -136,12 +132,11 @@ def fine_tune(model, train_data, val_data, selected_model):
         total = time.time() - total
         print("\nElapsed Time: " + str(datetime.timedelta(seconds=int(round((total))))))
         print("Average loss: " + str(train_loss/len(train_data)))
-        bar.finish()
 
         run_validation(model, val_data)
     
     print("**Ended Fine Tune**\n")
-    utils.checkpoint(model, optimizer, scheduler, selected_model)
+    utils.checkpoint(model, optimizer, scheduler, epoch, batch_n ,selected_model, model_path)
 
 def run_validation(model, val_data):
     model.eval()
@@ -150,10 +145,7 @@ def run_validation(model, val_data):
     val_loss = 0
 
     print("\n**Running Validation batches**")
-    bar = IncrementalBar('Batch', max = len(val_data))
     for batch in val_data:
-        bar.next()
-
         with torch.no_grad():
             loss, logits = model(batch[0].to(device), token_type_ids=None,
                                 attention_mask=batch[1].to(device), 
@@ -162,8 +154,7 @@ def run_validation(model, val_data):
             logits = logits.detach().cpu().numpy()
             val_loss += loss.item()
             val_acc += utils.accuracy(batch[2].numpy(), logits)
-        
-    bar.finish()
+    
     print("Average validation loss: " + str(val_loss/len(val_data)))
     print("Average validation accuracy: " + str(val_acc/len(val_data)))
 
@@ -203,7 +194,32 @@ def testing(test_data, selected_model):
     print("Average test accuracy: " + str(test_acc/len(test_data)))
     print("**Ended Testing**")
 
+def load_checkpoint(model, optimizer, scheduler, filename):
+    # Note: Input model & optimizer should be pre-defined.  This routine only updates their states.
+    start_epoch = 0
+    filename = dir_path + filename + "_finetuned.pth"
+    if os.path.isfile(filename):
+        print("=> loading checkpoint '{}'".format(filename))
+        checkpoint = torch.load(filename)
+        start_epoch = checkpoint['epoch']
+        model.load_state_dict(checkpoint['model_state_dict'])
+        optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
+        scheduler.load_state_dict(checkpoint['scheduler_state_dict'])
+        batch_num = checkpoint['batch_num']
+        print("=> loaded checkpoint '{}' (epoch {})"
+                  .format(filename, checkpoint['epoch']))
+    else:
+        print("=> no checkpoint found at '{}'".format(filename))
+
+    return model, optimizer, scheduler, start_epoch, batch_num
+
 if __name__ == "__main__":
+    dataloader_path = 'H:\\AdvancedMachineLearningCW\\Deep\\'
+    model_path = 'H:\\AdvancedMachineLearningCW\\Deep\\'
+    use_start_dataloader = True
+    use_checkpoint = True
+    dir_path = os.path.dirname(os.path.realpath(__file__)) + "\\"
+
     arg = sys.argv
     if len(arg) == 1:
         raise Exception("No model specification provided")
@@ -215,11 +231,20 @@ if __name__ == "__main__":
     df = utils.get_data()
 
     if selected_model == "BERT":
-        model, tokenizer = setup_BERT()
+        model, tokenizer = utils.setup_BERT()
     else:
-        model, tokenizer = setup_XLNet()
+        model, tokenizer = utils.setup_XLNet()
 
-    ids, masks, labels = tokenize(df, tokenizer)
-    train_data, val_data, test_data = split_data(ids, masks, labels)
-    fine_tune(model, train_data, val_data, selected_model)
+    if use_start_dataloader or use_checkpoint:
+        train_data = torch.load(dir_path + 'start_train_dataloader.pth')
+        val_data = torch.load(dir_path + 'start_val_dataloader.pth')
+        test_data = torch.load(dir_path + 'start_test_dataloader.pth')
+    else:
+        ids, masks, labels = tokenize(df, tokenizer)
+        train_data, val_data, test_data = split_data(ids, masks, labels)
+    if not use_start_dataloader:
+        torch.save(train_data, dir_path + 'start_train_dataloader.pth')
+        torch.save(val_data, dir_path + 'start_val_dataloader.pth')
+        torch.save(test_data, dir_path + 'start_test_dataloader.pth')
+    fine_tune(model, train_data, val_data, selected_model, use_checkpoint)
     testing(test_data, selected_model)
--- Deep/utils.py
+++ Deep/utils.py
@@ -2,17 +2,40 @@ import numpy as np
 from sklearn.metrics import matthews_corrcoef
 import pandas as pd
 import torch
+from transformers import XLNetModel, XLNetTokenizer, XLNetForSequenceClassification
+from transformers import BertModel, BertTokenizer, BertForSequenceClassification
 
-def checkpoint(model, optimizer, scheduler, selected_model):
-    torch.save({
-            'model_state_dict': model.state_dict(),
-            'optimizer_state_dict': optimizer.state_dict(),
-            'scheduler_state_dict': scheduler.state_dict(),
-    }, selected_model+"_finetuned.pth")
+device = "cuda:0" if torch.cuda.is_available() else "cpu"
+num_classes = 5
 
+def setup_BERT():
+    tokenizer = BertTokenizer.from_pretrained('bert-base-cased', do_lower_case=True)
+    model = BertForSequenceClassification.from_pretrained("bert-base-cased", num_labels=num_classes, output_attentions = False, output_hidden_states = False)
+    #model.classifier = nn.Linear(768, num_classes)
 
+    #for param in model.parameters():
+    #    param.requires_grad = False
+    
+    #model.classifier.weight.requires_grad = True #unfreeze last layer weights
+    #model.classifier.bias.requires_grad = True #unfreeze last layer biases
+    model = model.to(device)
 
-def checkpoint_2(model, optimizer,scheduler, epoch, batch_num ,selected_model, save_path):
+    return model, tokenizer
+
+def setup_XLNet():
+    tokenizer = XLNetTokenizer.from_pretrained('xlnet-base-cased', do_lower_case=True)
+    model = XLNetForSequenceClassification.from_pretrained("xlnet-base-cased", num_labels=num_classes, output_attentions = False, output_hidden_states = False)
+    #model.logits_proj = nn.Linear(768, num_classes)
+
+    #for param in model.parameters():
+    #    param.requires_grad = False
+    #model.logits_proj.weight.requires_grad = True #unfreeze last layer weights
+    #model.logits_proj.bias.requires_grad = True #unfreeze last layer biases
+    model = model.to(device)
+
+    return model, tokenizer
+
+def checkpoint(model, optimizer,scheduler, epoch, batch_num ,selected_model, save_path):
     torch.save({
             'model_state_dict': model.state_dict(),
             'epoch': epoch,
@@ -25,7 +48,6 @@ def get_data():
     df = pd.read_csv('H:\\AdvancedMachineLearningCW\\new_clean_sm_100000.csv', keep_default_na=False)
     df = df[df['reviewText'].notna()]
     df = df.rename(columns={'Unnamed: 0': 'Id'})
-    #df = df[:1000]
 
     return df
 
